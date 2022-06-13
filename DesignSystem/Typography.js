e1f4f56699de4125154adcff1e45239617f54db4
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-06-14 01:51:22 +0500

    Project Structure with basic Design System implemented

---
+++ DesignSystem/Typography.js
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  Header_24pt: {
+    fontSize: 24,
+    fontFamily: 'Inter',
+  },
+  Header_20pt: {
+    fontSize: 20,
+    fontFamily: 'Inter',
+  },
+  Header_16pt: {
+    fontSize: 16,
+    fontFamily: 'Inter',
+  },
+  Header_14pt: {
+    fontSize: 14,
+    fontFamily: 'Inter',
+  },
+  Header_12pt: {
+    fontSize: 12,
+    fontFamily: 'Inter',
+  },
+  Header_10pt: {
+    fontSize: 10,
+    fontFamily: 'Inter',
+  },
+  Text_14pt: {
+    fontSize: 14,
+    fontFamily: 'Roboto',
+  },
+  Text_12pt: {
+    fontSize: 12,
+    fontFamily: 'Roboto',
+  },
+  Text_10pt: {
+    fontSize: 10,
+    fontFamily: 'Roboto',
+  },
+  Text_8pt: {
+    fontSize: 8,
+    fontFamily: 'Roboto',
+  },
+});
