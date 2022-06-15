f938b67b44f48d5bd8e78e3a761d5266f4b0d72e
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-06-15 20:48:42 +0500

    Installed React Navigation and Tested Basic Development

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Typography from './DesignSystem/Typography';
 import {Colors} from './DesignSystem/Colors';
@@ -6,46 +7,19 @@ import LightTheme from './DesignSystem/LightTheme';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import Header from './Components/Header';
+import TextColors from './Screens/TextColors';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={LightTheme.Light}>
-      <Header></Header>
-      <View style={styles.flex}>
-        <Text style={[Typography.Header_24pt, {color: Colors.Primary1}]}>
-          Hello
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Primary2}]}>
-          Hello
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Primary3}]}>
-          Hello
-        </Text>
-      </View>
-      <View style={styles.flex}>
-        <Text style={[Typography.Header_24pt, {color: Colors.Secondary1}]}>
-          Doctor
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Secondary2}]}>
-          Doctor
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Secondary3}]}>
-          Doctor
-        </Text>
-      </View>
-
-      <View style={styles.flex}>
-        <Text style={[Typography.Header_24pt, {color: Colors.Ascent1}]}>
-          Haris
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Ascent2}]}>
-          Haris
-        </Text>
-        <Text style={[Typography.Header_24pt, {color: Colors.Ascent3}]}>
-          Haris
-        </Text>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Header" component={TextColors} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- android/app/src/main/java/com/madproject/MainActivity.java
+++ android/app/src/main/java/com/madproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.madproject;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "MadProject";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
