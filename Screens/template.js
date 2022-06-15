f0532dfeade4f0d18e582be7b734533cf4fea4ad
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-06-15 21:19:43 +0500

    Added Monochrome Colors

---
+++ Screens/template.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import Typography from '../DesignSystem/Typography';
+import {Colors} from '../DesignSystem/Colors';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+const template = ({navigation, route}) => {
+  return (
+    <View>
+      <Text style={{color: 'black'}}>template</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  flex: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+});
+
+export default template;
